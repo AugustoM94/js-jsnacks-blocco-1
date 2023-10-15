e6e025bfba1d0ee17d23c057b3901ea22dbeabd4
AugustoM94 <email@github.com>
2023-10-15 18:28:42 +0200

    Project completed

---
+++ La coda dell'Array/script2.js
@@ -0,0 +1,67 @@
+
+
+
+/*Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e generando tanti numeri casuali da 1 a 100 per quanti sono gli 
+elementi da inserire.
+Stampiamo poi gli ultimi 5 elementi dell'Array*/
+
+// Chiediamo all'utente quanti elementi dovrà contenere l'array
+/* let numElementi = parseInt(document.getElementById('num').value);
+
+// Verifichiamo se l'input dell'utente è un numero valido
+if (isNaN(numElementi) || numElementi <= 0) {
+  alert("Inserisci un numero valido superiore a 0.");
+} else {
+  // Creiamo l'array con numeri casuali da 1 a 100
+  const array = [];
+  for (let i = 0; i < numElementi; i++) {
+    array.push(Math.floor(Math.random() * 100) + 1);
+  }
+
+  // Stampiamo gli ultimi 5 elementi dell'array
+  const ultimi5Elementi = array.slice(-5);
+  console.log("Gli ultimi 5 elementi dell'array sono:", ultimi5Elementi);
+
+  // Chiediamo all'utente quanti di questi elementi vuole stampare
+  const numElementidaStampare = parseInt(document.getElementById('num').value);
+
+  if (!isNaN(numElementiDaStampare) && numElementiDaStampare >= 1 && numElementiDaStampare <= 5) {
+    const elementiDaStampare = ultimi5Elementi.slice(0, numElementiDaStampare);
+    console.log("Gli ultimi", numElementiDaStampare, "elementi dell'array sono:", elementiDaStampare);
+  } else {
+    console.log("Inserisci un numero valido tra 1 e 5.");
+  }
+}
+ */
+let generaArray = document.querySelector('#GeneraArray');
+let stampaNumeri = document.querySelector('#stampaNumeri');
+let arrayGenerato = [];
+
+generaArray.addEventListener('click', function () {
+    const numElementi = parseInt(document.getElementById('numElementi').value);
+
+    if (isNaN(numElementi)) {
+        alert("Inserisci un numero valido.");
+        return;
+    }
+
+    arrayGenerato = [];
+    for (let i = 0; i < numElementi; i++) {
+        arrayGenerato.push(Math.floor(Math.random() * 100) + 1);
+    }
+    document.getElementById('risultato').innerHTML = "Array generato: " + arrayGenerato;
+})
+
+stampaNumeri.addEventListener('click',function () {
+    const numStampa = parseInt(document.getElementById('numStampa').value);
+
+    if (isNaN(numStampa) || numStampa <= 0) {
+        alert("Inserisci un numero valido e maggiore di 0.");
+        return;
+}
+
+    const ultimiNumeri = arrayGenerato.slice(-numStampa);
+    document.getElementById('risultato').innerHTML = "Ultimi " + numStampa + " elementi dell'array: " + ultimiNumeri;
+})
+
+
---
+++ array-lista/script1.js
@@ -0,0 +1,29 @@
+
+
+   const frigo = [
+      'banana',
+      'mela',
+      'pera',
+      'ciliegia',
+      'arancia',
+      'mandarino',
+      'cocomero',
+      'limone',
+      'fragola',
+  
+     ]
+     document.getElementById("Frigo").innerHTML = frigo.join(', ');
+     console.log(frigo);
+     
+     let trovato = false;
+     let msg;
+     
+     for (let i = 0; i < frigo.length; i++) {
+       if (frigo[i] === 'cocomero') {
+         trovato = true;
+         msg = "Trovato! Devo solo preparare il cocktail.";
+         break; // Esci dal ciclo una volta trovato il cocomero
+       } else {
+         msg = "Oh no, devo uscire a comprare il cocomero!";
+       }
+     }
